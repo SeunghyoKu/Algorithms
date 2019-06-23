6d67ea53f623c1ac9a30510a3e58310eff4596b7
Seunghyo Ku <43411599+SeunghyoKu@users.noreply.github.com>
2019-06-24 03:18:02 +0900

    Create 1066.py

---
+++ CodeUp/Basic100/1066.py
@@ -0,0 +1,21 @@
+#세 정수 a, b, c가 입력되었을 때, 짝 even(짝)/odd(홀)을 출력해보자.
+
+a, b, c = input().split(" ")
+a = int(a)
+b = int(b)
+c = int(c)
+
+if a%2 == 0:
+  print("even")
+else:
+  print("odd")
+  
+if b%2 == 0:
+  print("even")
+else:
+  print("odd")
+  
+if c%2 == 0:
+  print("even")
+else:
+  print("odd")
