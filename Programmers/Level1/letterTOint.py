90c18c8be6fabec401e12b064dfbe3e90fae4548
Seunghyo Ku <43411599+SeunghyoKu@users.noreply.github.com>
2019-07-09 03:53:09 +0900

    Create letterTOint.py

---
+++ Programmers/Level1/letterTOint.py
@@ -0,0 +1,12 @@
+#문자열을 정수로 바꾸기
+#https://programmers.co.kr/learn/courses/30/lessons/12925?language=python3
+
+#문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+
+
+def solution(s):
+    answer = int(s)
+    return answer
+    
+#나의 풀이
+#간단히 int()함수를 사용해서 문자열을 정수형으로 바꾸어주었다. 고마워요 파이썬!
