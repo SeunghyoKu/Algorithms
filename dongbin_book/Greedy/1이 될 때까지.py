9b6ec6eca49ebea6aeb97b5dfa752819d320fc6f
Seunghyo Ku <43411599+SeunghyoKu@users.noreply.github.com>
2021-03-13 19:28:46 +0900

    Create 1이 될 때까지.py

---
+++ dongbin_book/Greedy/1이 될 때까지.py
@@ -0,0 +1,14 @@
+# Dongbin Book - Greedy Algorithm
+# 4. 1이 될 때까지 
+
+n, k = map(int, input().split(" "))
+count = 0
+
+while n > 1:
+  if n % k == 0:
+    n /= k
+  else:
+    n -= 1
+  count += 1
+
+print(count)
